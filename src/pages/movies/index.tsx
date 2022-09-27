c47479c98f9105b94f36c52bc962ab7a959198c3
Adrian Trujillo <adrian.td96@gmail.com>
2022-09-26 21:33:24 -0500

    added index for /movies

---
+++ src/pages/movies/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface MoviesHomeProps {
+  id: string;
+}
+
+const MoviesHome: React.FC<MoviesHomeProps> = ({}) => {
+  return (
+    <div>
+      <h1>Movies</h1>
+    </div>
+  );
+};
+
+export default MoviesHome;
