361d105e29c7cdd719ab6f7fd1cad23004c09416
Adrian Trujillo <adrian.td96@gmail.com>
2022-09-26 21:33:35 -0500

    added index for /people

---
+++ src/pages/people/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface PeopleHomeProps {
+  id: string;
+}
+
+const PeopleHome: React.FC<PeopleHomeProps> = ({}) => {
+  return (
+    <div>
+      <h1>People</h1>
+    </div>
+  );
+};
+
+export default PeopleHome;
