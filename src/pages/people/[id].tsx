1e103e049d4c53a68ad24d367ea7a78a6a698dc6
Adrian Trujillo <adrian.td96@gmail.com>
2022-09-26 21:34:56 -0500

    added dynamic /people/[id]

---
+++ src/pages/people/[id].tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface PersonDetailsProps {
+  id: string;
+}
+
+const PersonDetails: React.FC<PersonDetailsProps> = ({}) => {
+  return (
+    <div>
+      <h1>PersonDetails</h1>
+    </div>
+  );
+};
+
+export default PersonDetails;
