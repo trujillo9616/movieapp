61ed71595cc5d7230528fad0f43f21ce308f0e37
Adrian Trujillo <adrian.td96@gmail.com>
2022-09-26 21:33:51 -0500

    added index for /tv

---
+++ src/pages/tv/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface TvShowsHomeProps {
+  id: string;
+}
+
+const TvShowsHome: React.FC<TvShowsHomeProps> = ({}) => {
+  return (
+    <div>
+      <h1>TV Shows</h1>
+    </div>
+  );
+};
+
+export default TvShowsHome;
