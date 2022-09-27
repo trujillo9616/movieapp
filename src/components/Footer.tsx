0b26c67299bbba6a2d193e828fe849328ee85741
Adrian Trujillo <adrian.td96@gmail.com>
2022-09-26 21:30:20 -0500

    added Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>MovieDB</strong> by Adrian Trujillo.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
