beb6dda17293e9e9238bc2b81e3ff972a6d8f017
Adrian Trujillo <adrian.td96@gmail.com>
2022-09-26 21:32:12 -0500

    added Slider component

---
+++ src/components/Slider.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import MovieCard from "./MovieCard";
+import TvShowCard from "./TvShowCard";
+
+interface SliderProps {
+  title: string;
+  data: any;
+}
+
+const SimpleSlider: React.FC<SliderProps> = ({ title, data }) => {
+  const movie: boolean = data[0]?.name ? false : true;
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 8,
+    swipeToSlide: true,
+  };
+  return (
+    <div className="">
+      <h2>{title}</h2>
+      <Slider {...settings}>
+        {data.map((item: any, index: number) => {
+          return (
+            <div key={index} className="mx-2">
+              {movie ? (
+                <MovieCard
+                  id={item.id}
+                  title={item.title}
+                  poster_path={item.poster_path}
+                  release_date={item.release_date}
+                  genre_ids={item.genre_ids}
+                  vote_average={item.vote_average}
+                />
+              ) : (
+                <TvShowCard
+                  id={item.id}
+                  name={item.name}
+                  poster_path={item.poster_path}
+                  first_air_date={item.first_air_date}
+                  genre_ids={item.genre_ids}
+                  vote_average={item.vote_average}
+                />
+              )}
+            </div>
+          );
+        })}
+      </Slider>
+    </div>
+  );
+};
+
+export default SimpleSlider;
