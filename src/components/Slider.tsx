b1748981ca8d433abf447213226f6e22c1246f31
Adrian Trujillo <adrian.td96@gmail.com>
2022-09-28 22:59:59 -0500

    changed some of the options of the Slider component to ensure a good rendering

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -17,37 +17,73 @@ const SimpleSlider: React.FC<SliderProps> = ({ title, data }) => {
     speed: 500,
     slidesToShow: 8,
     swipeToSlide: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    autoplayHoverPause: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 6,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 980,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          initialSlide: 4,
+        },
+      },
+      {
+        breakpoint: 680,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
   };
   return (
-    <div className="">
-      <h2>{title}</h2>
-      <Slider {...settings}>
-        {data.map((item: any, index: number) => {
-          return (
-            <div key={index} className="mx-2">
-              {item.title ? (
-                <MovieCard
-                  id={item.id}
-                  title={item.title}
-                  poster_path={item.poster_path}
-                  release_date={item.release_date}
-                  genre_ids={item.genre_ids}
-                  vote_average={item.vote_average}
-                />
-              ) : (
-                <TvShowCard
-                  id={item.id}
-                  name={item.name}
-                  poster_path={item.poster_path}
-                  first_air_date={item.first_air_date}
-                  genre_ids={item.genre_ids}
-                  vote_average={item.vote_average}
-                />
-              )}
-            </div>
-          );
-        })}
-      </Slider>
+    <div className="py-4">
+      <h2 className="py-4">
+        <span className="mx-6 text-sm md:text-xl leading-normal font-extrabold text-gray-200 text-ellipsis">
+          {title}
+        </span>
+      </h2>
+      <div>
+        <Slider {...settings}>
+          {data.map((item: any, index: number) => {
+            return (
+              <div key={index}>
+                {item.title ? (
+                  <MovieCard
+                    id={item.id}
+                    title={item.title}
+                    poster_path={item.poster_path}
+                    release_date={item.release_date}
+                    genre_ids={item.genre_ids}
+                    vote_average={item.vote_average}
+                  />
+                ) : (
+                  <TvShowCard
+                    id={item.id}
+                    name={item.name}
+                    poster_path={item.poster_path}
+                    first_air_date={item.first_air_date}
+                    genre_ids={item.genre_ids}
+                    vote_average={item.vote_average}
+                  />
+                )}
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
     </div>
   );
 };
