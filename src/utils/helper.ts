5f39e9e4d01dc0dc99a184f596874a256910546d
Adrian Trujillo <adrian.td96@gmail.com>
2022-10-20 23:53:38 -0500

    added helper function to get people zodiac signs

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import { genresById } from "./obj";
 import { CountryType, VideoType } from "./types";
 
@@ -26,4 +27,43 @@ export function getTrailerKey(videos: VideoType): string {
 
 export function getWatchProviders(watchProviders: any): any {
   return watchProviders.results.US;
+}
+
+export function getZodiacSign(date: string | null): string | undefined {
+  if (!date) return undefined;
+  const [year, _month, _day] = date.split("-");
+  const zodiacSigns = [
+    "Aries ♈️",
+    "Taurus ♉️",
+    "Gemini ♊️",
+    "Cancer ♋️",
+    "Leo ♌️",
+    "Virgo ♍️",
+    "Libra ♎️",
+    "Scorpio ♏️",
+    "Sagittarius ♐️",
+    "Capricorn ♑️",
+    "Aquarius ♒️",
+    "Pisces ♓️",
+  ];
+  const zodiacSignsDays = [
+    ["03-21", "04-19"],
+    ["04-20", "05-20"],
+    ["05-21", "06-20"],
+    ["06-21", "07-22"],
+    ["07-23", "08-22"],
+    ["08-23", "09-22"],
+    ["09-23", "10-22"],
+    ["10-23", "11-21"],
+    ["11-22", "12-21"],
+    ["12-22", "01-19"],
+    ["01-20", "02-18"],
+    ["02-19", "03-20"],
+  ];
+
+  for (let i = 0; i < zodiacSignsDays.length; i++) {
+    const [start, end]: any = zodiacSignsDays[i];
+    const zodiac = moment(`${year}-${start}`).isSameOrBefore(date) && moment(`${year}-${end}`).isSameOrAfter(date);
+    if (zodiac) return zodiacSigns[i];
+  }
 }
