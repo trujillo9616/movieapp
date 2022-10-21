5799fc01a7506f10d0b502dafbc368ef2899417b
Adrian Trujillo <adrian.td96@gmail.com>
2022-10-20 22:38:04 -0500

    refactor footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,25 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer">
-      <div className="content has-text-centered">
-        <p>
-          <strong>MovieDB</strong> by Adrian Trujillo.
-        </p>
-      </div>
-    </footer>
+    <div className="py-10">
+      <footer>
+        <div className="flex flex-col items-center justify-center py-4">
+          <div className="flex items-center space-x-2 text-slate-400">
+            <p>
+              Made with <span className="text-red-500">❤</span> by{" "}
+              <a
+                href="https://www.linkedin.com/in/a-trujillo/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Adrian Trujillo
+              </a>
+              . All rights reserved © 2022.
+            </p>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 };
 
